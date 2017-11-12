e2a0e1068e7336c108a33e4087c3cfde3935aeb5
ChrisR5 <chris.robson15@gmail.com>
2017-11-12 14:11:02 +0000

    Changed/Added to the login page

--- home.php
+++ home.php
@@ -13,8 +13,9 @@ require('./conf/sescheck.php');
         <link rel="shortcut icon" href="./img/favicon.png" type="image/x-icon">
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     </head>
+
     <body>
-    <div class="mdl-layout mdl-js-layout">
+    <div class="mdl-layout mdl-js-layout mdl-color--grey-100">
         <header class="mdl-layout__header">
             <div class="mdl-layout__header-row">
                 <span class="mdl-layout-title">HackPanel: BullHacks 2018</span>
@@ -31,16 +32,16 @@ require('./conf/sescheck.php');
 
 					<ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
     					for="menu-lower-right">
-						<a class="mdl-menu__item" style="text-decoration:none; color:black;" href="./logout.php">Log Out</a>
-						<a class="mdl-menu__item" style="text-decoration:none; color:black;" href="./settings.php">Settings</a>
-						<a class="mdl-menu__item" style="text-decoration:none; color:black;" href="https://airtable.com/shrCr68AzwBT4XyQD" target="_blank">Found a Bug?</a>
+						<a class="mdl-menu__item" href="./logout.php">Log Out</a>
+						<a class="mdl-menu__item" href="./settings.php">Settings</a>
+						<a class="mdl-menu__item" href="https://airtable.com/shrCr68AzwBT4XyQD" target="_blank">Found a Bug?</a>
 					</ul>
                 </nav>
             </div>
         </header>
 		
         <div class="mdl-layout__drawer">
-            <span class="mdl-layout-title">HackPanel</span>
+            <img class="mdl-layout-title logo" src="./img/bullhackswideborder.png" />
             <nav class="mdl-navigation">
                 <a class="mdl-navigation__link" href="./home.php">Home</a>
                 <a class="mdl-navigation__link" href="./attendees.php">Attendees</a>
